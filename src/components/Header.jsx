d989165d08546cc4bebf17a437594c28bcf8d77b
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-02-19 13:31:38 +0100

    ADD: header

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from "../assets/img/dc-logo.png"
+
+const Header = function () {
+    return (
+        <header>
+            <div className="container">
+
+                <figure>
+                    <img src={logo} alt="Logo" />
+                </figure>
+
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a className="active" href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+const Main = function () {
+
+}
+
+export default Main
